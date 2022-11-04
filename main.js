d087a3ce4c20967130d6eb5ac61d48f076f90030
LucasLoureiro08 <116582484+LucasLoureiro08@users.noreply.github.com>
2022-11-04 10:43:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function createGame (player1, hour, player2) {
+return `
+            <li>
+                <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+                <strong>${hour}</strong>
+                <img src="./assets/icon-${player2}.svg" alt="Bandeira do ${player2}">
+            </li>
+`
+
+}
+
+let delay = -0.8;
+function createCard(date, day, games) {
+    delay = delay +0.8;
+    return `
+    <div class="card" style="animation-delay: ${delay}s"> <!-- As classes podem ter identificadores iguais, e novos atributos irão ser aplicadas as classes com o mesmo identificador-->
+        <h2>${date} <span>${day}</span></h2>
+                    
+        <ul>
+            ${games}
+        </ul>
+            </div>
+    `
+}
+
+document.querySelector("#cards").innerHTML = 
+    
+    createCard('24/11', "quinta",createGame('brazil', "16:00", "serbia"))
+    +
+    createCard('28/11', "segunda",createGame('brazil', "13:00", "switzerland") + createGame('portugal', "16:00", "uruguay"))
+    +
+    createCard('02/12', "sexta",createGame('brazil', "16:00", "cameroon"))
